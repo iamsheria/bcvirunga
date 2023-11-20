51444b8777bc079bb3286c76876d631469bc07a5
iamsheria <69418475+iamsheria@users.noreply.github.com>
2023-11-20 09:50:29 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- font awesome cdn link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    
+    <!--links style sheet-->
+    <link rel="stylesheet" href="css/styles.css">
+    <title>bcvirunga</title>
+</head>
+<body>
+    
+    <header>
+
+        <div class="container">
+            <!--logo styling-->
+            <div class="logo_brand">
+                
+                <img src="./assets/logos/BC_Virunga_logo.png" alt="">
+
+            </div>
+
+            <!--icons naviation-->
+
+            <div class="icons">
+                
+                <input type="checkbox" name="" class="check" id="toggle">
+                <label for="toggle"  id="menu-btn">
+                    <div class="fas fa-bars"></div>
+                </label>
+            </div>
+    
+
+            <!--navigation menu list-->
+
+            <div class="menu_nav_list">
+
+                <ul class="nav_ul" id="nav_ul">
+
+                    <li class="nav-link">
+                        <a href="#">acceuil</a>
+                    </li>
+
+                    <li class="nav-link">
+                        <a href="#">annonces</a>
+                    </li>
+
+                    <li class="nav-link">
+                        <a href="#">galerie</a>
+                    </li>
+
+                    <li class="nav-link">
+                        <a href="#">boutique</a>
+                    </li>
+
+                    <li class="nav-link">
+                        <a href="#">blogs</a>
+                    </li>
+
+                    <li class="nav-link">
+                        <a href="#">contact</a>
+                    </li>
+
+                    <li class="nav-link">
+                        <a href="#">a propos</a>
+                    </li>
+
+                    
+
+                </ul>
+
+                <a href="#" class="main_btn">faire un don</a>
+            </div>
+        </div>
+
+    </header>
+
+
+    <!-- home section starts  -->
+
+    <section class="home" id="home">
+
+        <div class="content">
+            <h3>Site officiel <span>bc Virunga</span> </h3>
+            <p>Bienvenu sur le site officiel du Basketball Club VIRUNGA, Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat labore, sint cupiditate distinctio tempora reiciendis.</p>
+            <a href="#" class="main_btn">Explorer</a>
+        </div>
+
+    </section>
+
+    <!-- home section ends  -->
+
+
+</body>
+</html>
